80b851e0d23b55c1cb87aff027bf5e5ecfb75d02
Ma <101021254+CodeWithMa@users.noreply.github.com>
2026-06-20 12:14:27 +0200

    Remove ZPLUGINDIR fallback, rely on ZSH_CONFIG_DIR

--- plugins.zsh
+++ plugins.zsh
@@ -2,7 +2,7 @@
 
 function plugin-load() {
   local plugin_dir plugin_name initfile initfiles
-  local ZPLUGINDIR=${ZPLUGINDIR:-$ZSH_CONFIG_DIR/plugins}
+  local ZPLUGINDIR=$ZSH_CONFIG_DIR/plugins
 
   for plugin_name in "$@"; do
     plugin_dir=$ZPLUGINDIR/$plugin_name
