31aad6f9b25515efa29b1f811892c756d49235a9
Ma <101021254+CodeWithMa@users.noreply.github.com>
2026-07-12 07:08:21 +0200

    Also bind alternative up and down to history-substring-search

--- zsh-history-substring-search.zsh
+++ zsh-history-substring-search.zsh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 # bindkeys for arrow up and down
 # for https://github.com/zsh-users/zsh-history-substring-search
-#bindkey '\eOA' history-substring-search-up
-#bindkey '\eOB' history-substring-search-down
+bindkey '\eOA' history-substring-search-up
+bindkey '\eOB' history-substring-search-down
 bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
 
